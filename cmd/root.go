4f2aba438de02779dbbe696cf98a7d4d63a5e890
Joshua Gilman <joshuagilman@gmail.com>
2020-04-30 20:39:31 -0700

    Adds flags and details to root command

--- cmd/root.go
+++ cmd/root.go
@@ -25,17 +25,14 @@ import (
 )
 
 var cfgFile string
+var vaultToken string
+var vaultAddress string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gcli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A CLI utility for managing the Gilman lab (glab)",
+	Long: ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
@@ -53,15 +50,20 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
+	// Viper flags
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gcli.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Vault flags
+	rootCmd.PersistentFlags().StringVar(&vaultAddress, "vault-address", "", "Vault server address (defaults to VAULT_ADDR)")
+	err := viper.BindPFlag("", rootCmd.PersistentFlags().Lookup("role"))
+
+	rootCmd.PersistentFlags().StringVar(&vaultToken, "vault-token", "", "Vault token (defaults to VAULT_TOKEN)")
+	err = viper.BindPFlag("", rootCmd.PersistentFlags().Lookup("role"))
+
+	if err != nil {
+		fmt.Println("Error binding to flags:", err)
+		os.Exit(1)
+	}
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -82,6 +84,7 @@ func initConfig() {
 		viper.SetConfigName(".gcli")
 	}
 
+	viper.SetEnvPrefix("VCLI")
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
